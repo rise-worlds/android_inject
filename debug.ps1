d63a8e2575fb65ef46c3bf96ee97dc3aa314eea3
rise <rise.worlds@outlook.com>
2023-03-08 19:08:07 +0800

    add debug config

---
+++ debug.ps1
@@ -0,0 +1,5 @@
+adb shell "rm /data/local/tmp/inject_service; rm /data/local/tmp/libUnityCheat.so;"
+adb push build/inject_service build/libUnityCheat.so /data/local/tmp
+adb shell "chmod 777 /data/local/tmp/inject_service"
+adb forward tcp:9090 tcp:9090
+adb shell "su -c '/data/local/tmp/lldb-server platform --listen *:9090 --server' &"
