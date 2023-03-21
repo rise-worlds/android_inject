8daf1cb10290efa31efbf03da710405154172eb8
rise <rise.worlds@outlook.com>
2023-03-21 14:49:53 +0800

    update

--- debug.ps1
+++ debug.ps1
@@ -1,6 +1,7 @@
 adb shell "su -c 'killall -9 inject_service'"
-adb shell "rm /data/local/tmp/inject_service; rm /data/local/tmp/libUnityCheat.so;"
-adb push build/inject_service build/libUnityCheat.so /data/local/tmp
+adb shell "rm /data/local/tmp/inject_service; rm /data/local/tmp/libinject_speed.so;"
+adb push build/inject_service build/libinject_speed.so /data/local/tmp
 adb shell "chmod 777 /data/local/tmp/inject_service"
 adb forward tcp:9090 tcp:9090
-adb shell "su -c '/data/local/tmp/lldb-server platform --listen *:9090 --server' &"
+adb forward tcp:10086 tcp:10086
+adb shell "su -c '/data/local/tmp/lldb-server platform --listen *:9090 --server &' &"
